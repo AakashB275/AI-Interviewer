888fed7e45686b534cecd33e1c468c77cee3ea29
Aakash Borji <aakashborji7@gmail.com>
2025-08-21 23:11:48 +0530

    isLoggedIn and few more changes

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,9 @@ const path = require("path");
 const usersRouter = require("./src/routes/usersRouter");
 const config = require("config");
 const dbauth = config.get("MONGODB_URI");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
 console.log("Using DB URI:", dbauth);
 
 
@@ -14,6 +17,14 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(
+    expressSession({
+        resave:false,
+        saveUninitialized: false,
+        secret: process.env.EXPRESS_SESSION_SECRET,
+    })
+)
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const { generateToken }= require("../utils/generateTokens");
 
 module.exports.registerUser = async function (req, res) {
+    console.log("Registration request received:", req.body);
     try {
         let { email, userName, password, contact } = req.body;
 
@@ -41,3 +42,46 @@ module.exports.registerUser = async function (req, res) {
         return res.status(500).json({ error: err.message });
     }
 }
+
+module.exports.loginUser = async function(req, res){
+    try {
+        let { userName, password } = req.body;
+
+        let user = await userModel.findOne({ userName });
+        if (!user) {
+            return res.status(401).json({ error: "Username or Password incorrect" });
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password, function(err, result){
+            if(result) {
+                let token = generateToken(user);
+                res.cookie("token",token);
+                res.send("you can login");
+            }
+        });
+        
+        if (!isMatch) {
+            return res.status(401).json({ error: "Username or Password incorrect" });
+        }
+
+        // Generate token and send success response
+        let token = generateToken(user);
+        res.cookie("token", token);
+        
+        return res.status(200).json({
+            message: "Login successful",
+            user: {
+                id: user._id,
+                email: user.email,
+                userName: user.userName,
+                contact: user.contact
+            }
+        });
+
+    } catch (err) {
+        console.error("Error in login:", err.message);
+        return res.status(500).json({ error: err.message });
+    }
+}
+
